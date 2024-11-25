631dc20d73678b408b9096e4118e15be116abf01
rohit <rohit.selvakumaran@gmail.com>
2024-11-26 00:24:38 +0530

    add login register

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
-app.use('/api', userRoutes);
+app.use('/lcms/user', userRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
